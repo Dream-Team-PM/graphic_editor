f6c1104b162fdb49d273d7eac0b025d21f086518
Vladimir Iamshchikov <89137420014a@gmail.com>
2026-02-17 11:50:45 +0700

    Note: Added a skeleton of new Avalonia project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace graphic_editor.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace graphic_editor.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
