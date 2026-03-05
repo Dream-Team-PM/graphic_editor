fa0492d6d78e30cc89be1f515bb634761c01bf5c
Zavik <zav021104@gmail.com>
2026-03-05 20:13:31 +0700

    feat(IO): добавил сохранение и загрузку проектов

---
+++ IO/Dto/ProjectDto.cs
@@ -0,0 +1,10 @@
+namespace graphic_editor.IO.Dto;
+
+public class ProjectDto
+{
+    public string Version { get; set; } = "1.0";
+    public double Zoom { get; set; } = 1.0;
+    public double OffsetX { get; set; }
+    public double OffsetY { get; set; }
+    public List<LayerDto> Layers { get; set; } = new();
+}
---
+++ IO/ProjectFormat/IProjectFormat.cs
@@ -0,0 +1,8 @@
+namespace graphic_editor.IO.ProjectFormat;
+
+public interface IProjectFormat
+{
+    string FileExtension { get; }
+    Task SaveAsync(string fullPath, CanvasViewModel canvas);
+    Task LoadAsync(string fullPath, CanvasViewModel canvas);
+}
