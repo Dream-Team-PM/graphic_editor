c6aaf5ab0606f861eb7c12566c4f24deb145f317
BenzinX <kleontyev@list.ru>
2026-03-06 05:56:42 +0700

    Add the ability to change the stroke colour

--- Views/ColorPickerPopup.axaml.cs
+++ Views/ColorPickerPopup.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Media;
 using Avalonia.Interactivity;
+using Avalonia.Controls.Primitives;
 using System;
 
 namespace graphic_editor;
@@ -9,16 +10,22 @@ public partial class ColorPickerPopup : UserControl
 {
     public event Action<Color>? ColorSelected;
     public event Action? Cancelled;
-
+    
     private static readonly string[] Swatches = {
         "#FF4A90","#FF6B6B","#FF9F43","#FECA57",
         "#48DBFB","#3A86FF","#8338EC","#06D6A0",
         "#FFFFFF","#AAAAAA","#555555","#000000"
     };
-
+    
     public ColorPickerPopup()
     {
         InitializeComponent();
+        InitializeSwatches();
+    }
+    
+    private void InitializeSwatches()
+    {
+        SwatchPanel.Children.Clear();
         foreach (var hex in Swatches)
         {
             var b = new Border {
@@ -27,21 +34,50 @@ public partial class ColorPickerPopup : UserControl
                 Background = Brush.Parse(hex),
                 Cursor = new Avalonia.Input.Cursor(Avalonia.Input.StandardCursorType.Hand)
             };
-            b.PointerPressed += (_, _) => { HexInput.Text = hex.TrimStart('#'); };
+            b.PointerPressed += (_, _) => { 
+                HexInput.Text = hex.TrimStart('#'); 
+                UpdatePreviewFromHex();
+            };
             SwatchPanel.Children.Add(b);
         }
     }
-
-    private void HexInput_TextChanged(object? sender, TextChangedEventArgs e)
+    
+    public void SetColor(Color color)
+    {
+        _currentColor = color;
+        PreviewBorder.Background = new SolidColorBrush(color);
+        HexInput.Text = $"{color.R:X2}{color.G:X2}{color.B:X2}";
+    }
+    
+    private Color _currentColor;
+    
+    private void UpdatePreviewFromHex()
     {
         if (HexInput.Text?.Length == 6)
-            try { PreviewBorder.Background = Brush.Parse("#" + HexInput.Text); } catch { }
+        {
+            try
+            {
+                var color = Color.Parse("#" + HexInput.Text);
+                _currentColor = color;
+                PreviewBorder.Background = new SolidColorBrush(color);
+            }
+            catch { }
+        }
     }
-
+    
+    private void HexInput_TextChanged(object? sender, TextChangedEventArgs e)
+    {
+        UpdatePreviewFromHex();
+    }
+    
     private void Ok_Click(object? sender, RoutedEventArgs e)
     {
-        try { ColorSelected?.Invoke(Color.Parse("#" + HexInput.Text)); } catch { }
+        try 
+        { 
+            ColorSelected?.Invoke(_currentColor); 
+        } 
+        catch { }
     }
-
+    
     private void Cancel_Click(object? sender, RoutedEventArgs e) => Cancelled?.Invoke();
 }
