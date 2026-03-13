884b7fea29dc4d75207307a5f11b4c37c3db9756
Vladimir Iamshchikov <89137420014a@gmail.com>
2026-03-13 11:13:52 +0700

    Updated IO with saving in JPG/JPEG/PDF/BMP formats

--- IO/Mappers/FigureDtoMapper.cs
+++ IO/Mappers/FigureDtoMapper.cs
@@ -1,4 +1,4 @@
-using System.Drawing;
+﻿using System.Drawing;
 using graphic_editor.Geometry;
 using graphic_editor.IO.Dto;
 using graphic_editor.ViewModels;
@@ -26,7 +26,7 @@ public static class FigureDtoMapper
                 Width = r.Width,
                 Height = r.Height
             },
-            CircleViewModel c => new CircleDto
+			CircleViewModel c => new CircleDto
             {
                 Id = c.Id,
                 Name = c.Name,
