2c38938f1af8e2a53a116dc279149f070d13dbc7
Vladimir Iamshchikov <89137420014a@gmail.com>
2026-03-26 21:19:11 +0700

    Note: Updated Layer Logic, added documentation and added Text object

---
+++ Controls/Converters/IsGreaterThanOneConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace graphic_editor.Converters;
+
+public class IsGreaterThanOneConverter : IValueConverter
+{
+    public static readonly IsGreaterThanOneConverter Instance = new();
+    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is int count && count > 1;
+    }
+    
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
